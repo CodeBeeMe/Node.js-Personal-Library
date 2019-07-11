942e6f1d5b06942cb7733f1a9ebabe3b79f60c60
Cătălin Anghel-Ursu <cat.ang.dev@gmail.com>
2019-07-11 16:47:13 +0300

    Update api.js
    Completed all the required methods for the forms to work as intended. The books are added to the database. The list of all the books in the "library" collection on the database is retrieved without a hitch. The "commentcount" property displays the correct value based on the "comments" array's length. The comments are posted correctly based on the book _id property. The book delete based on _id works as intended. In case the library is empty will be displayed a "None" status message. The delete all books also works as intended. The project is fully functional. 9 out of 10 user stories are completed and all that remains is to write the functional_tests to fully complete the project.

--- routes/api.js
+++ routes/api.js
@@ -15,56 +15,87 @@ const ObjectId = require('mongodb').ObjectId;
 module.exports = (app, db) => {
   
   app.route('/api/books')
-    .get(function (req, res){
+    .get(function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookId, "title": book_title, "commentcount": num_of_comments },...]
-    })
-    
+    db.collection("library")
+      .find({})
+      .toArray((err, books) => {
+      //getting the commentcount value looking at the length of the comments array
+      books.forEach(el => el.commentcount = el.comments.length);
+      err ? res.send(err) : res.send(books);
+      //console.log(books);
+    });
+  })  
+  
     .post(function (req, res){
     const title = req.body.title;
     //response will contain new book object including atleast _id and title
     
-    const bookEntry = {
-      title         : title,
-      comment_count : 0
-    } 
+    //Book object constructor
+    function Book() {
+      this.title  = title;
+      this.comments = [];
+      //this.commentcount  = this.comments.length; //getting the commentcount value looking at the length of the comments array
+    }
     
     if (!title) { //checking to see if the user entered a title in the required input field
       res.send('*required fields missing');
-    } else { //all fields are filled => proceed to add the issue to the database
-      db.collection("library").insertOne(bookEntry, (err, doc) => {
-        console.log('Book ' + title + ' has been successfully submitted into the library.');
-        console.log(bookEntry); 
-        err ? res.send(err) : res.json(bookEntry);
-        //db.close();
-      })
+    } else { //all fields are filled => proceed to add the book to the library database collection
+      db.collection("library")
+        .insertOne(new Book(), (err, doc) => {
+        err ? res.send(err) : res.send(new Book());
+        //console.log('Book ' + title + ' has been successfully submitted into the library.');
+        //console.log(bookEntry);
+      });
     }
-    
-    
-    })
-    
+  })
+      
     .delete(function(req, res){
       //if successful response will be 'complete delete successful'
+    db.collection("library")
+      .deleteMany({}, (err, doc) => {
+      err ? res.send('complete delete unsuccessful') : res.send('complete delete successful');
+      //console.log(doc);  
     });
-
-
+  });
 
   app.route('/api/books/:id')
     .get(function (req, res){
-      const bookId = req.params.id;
-      //json res format: {"_id": bookId, "title": book_title, "comments": [comment,comment,...]}
-    })
+    const bookId = req.params.id;
+    //json res format: {"_id": bookId, "title": book_title, "comments": [comment,comment,...]}
+    db.collection("library")
+      .find({_id: new ObjectId(bookId)})
+      .toArray((err, book) => {
+      book.length < 1 ? res.send('no book exists') : null;
+      err ? res.send(err) : res.send(book[0]);
+      console.log(book[0]);
+    });
+  })
     
     .post(function(req, res){
       const bookId = req.params.id;
       const comment = req.body.comment;
       //json res format same as .get
-    
+    db.collection("library")
+      .findOneAndUpdate(
+      {_id: new ObjectId(bookId)},
+      { $push: { comments: comment }},
+      (err, book) => {
+      err ? res.send(err) : res.send(book.value);
+      //console.log('Comment has been successfully submitted to the book.');
+      //console.log(book);
+      });    
     })
     
     .delete(function(req, res){
-      const bookId = req.params.id;
-      //if successful response will be 'delete successful'
+    const bookId = req.params.id;
+    //if successful response will be 'delete successful'
+    db.collection("library")
+      .deleteOne({_id: new ObjectId(bookId)}, (err, doc) => {
+      err ? res.send('delete unsuccessful') : res.send('delete successful');
+      //console.log(doc);  
     });
-  
+  });
+
 };
